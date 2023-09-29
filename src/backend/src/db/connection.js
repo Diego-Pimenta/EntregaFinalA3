0fb5939691b846606484b4178ab287d361b24117
Diego-Pimenta <diegopimenta888@gmail.com>
2023-09-29 02:58:28 -0300

    Vite installed and npm scrpits almost done

--- src/backend/src/db/connection.js
+++ src/backend/src/db/connection.js
@@ -1,10 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-let conn = null;
-
 /* = new Sequelize('postgres://user:pass@example.com:5432/dbname');
 a conexão com o postgres também pode ser realizada dessa forma */
-conn = new Sequelize('database', 'username', 'password', {
+const conn = new Sequelize('database', 'username', 'password', {
     host: 'localhost',
     dialect: 'postgres'
 });
@@ -15,3 +13,5 @@ try {
 } catch (err) {
     console.error('Unable to connect to the database:', err);
 }
+
+module.exports = {conn}
